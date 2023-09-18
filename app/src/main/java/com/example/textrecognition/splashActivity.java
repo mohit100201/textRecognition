eda05e555e9281d40da57df7ff8cffc7b508c1bc
Mohit Khandelwal <mohitkhandelwal9784@gmail.com>
2023-09-18 17:57:52 +0530

    first commit

---
+++ app/src/main/java/com/example/textrecognition/splashActivity.java
@@ -0,0 +1,18 @@
+package com.example.textrecognition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+
+
+    }
+}
